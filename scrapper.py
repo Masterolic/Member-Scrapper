3292592e13f66291cfa5fb9eba1f628386c7cb3f
Masterolic <rockingmallu8@gmail.com>
2023-10-24 20:46:24 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,14 @@
 from pyrogram import Client
+from dotenv import load_dotenv
+load_dotenv("config.env")
+from os import environ 
+if environ.get('app1'):
+   app1=Client(session_string)
+if environ.get('app2'):
+   app2=Client(session_string)
+if environ.get('app3'):
+   app3=Client(session_string)
+if environ.get('app4'):
+   app4=Client(session_string)
 
-app1=Client(session_string)
-app2=Client(session_string)
+
