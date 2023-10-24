fb739fd29ff6e0919c7bc443b92ef2d731b0e2c7
Masterolic <rockingmallu8@gmail.com>
2023-10-24 21:22:05 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -10,18 +10,22 @@ if environ.get('app3'):
    app3=Client(session_string)
 if environ.get('app4'):
    app4=Client(session_string)
+
 try:
    OWNER_ID=environ['OWNER_ID']
    CHANNEL_ID=environ['CHANNEL_ID']
    DUMP_ID=envitorn['DUMP_ID']
+   API_ID=environ['API_ID']
+   API_HASH=environ['API_HASH']
+   BOT_TOKEN=environ['BOT_TOKEN']
 except KeyError as e:
     print(f"Madantory variables are missing {e}")
-   
+bot=Client("SCRAPER",API_ID,API_HASH,BOT_TOKEN)   
 
-@Client.on_message(filters.command('start') & filters.user(OWNER_ID))
+@bot.on_message(filters.command('start') & filters.user(OWNER_ID))
 async def start(bot,m):
       try:
-          await m.edit("starting")
+          await m.reply("starting")
           print("Masterolic Member Scrapper Rolling")
           await bot.get_chat_members(CHANNEL_ID)
       
