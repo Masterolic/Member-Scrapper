8e9b6a1dcf759f08c98b1e02f8a2131e25d65266
Masterolic <rockingmallu8@gmail.com>
2023-10-24 20:56:20 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -10,5 +10,19 @@ if environ.get('app3'):
    app3=Client(session_string)
 if environ.get('app4'):
    app4=Client(session_string)
+try:
+   OWNER_ID=environ['OWNER_ID']
+   CHANNEL_ID=environ['CHANNEL_ID']
+   DUMP_ID=envitorn['DUMP_ID']
+except KeyError as e:
+    print(f"Madantory variables are missing {e}")
+   
 
-
+@Client.on_message(filters.command('start')
+async def start(bot,m):
+      try:
+          await m.edit("starting")
+          print("Masterolic Member Scrapper Rolling")
+          await bot.get_chat_members(CHANNEL_ID)
+      
+   
